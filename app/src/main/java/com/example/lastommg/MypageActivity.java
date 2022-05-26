7693828a9abc1a6c8fd613ce5d502afa70e88ac2
yeoul <yslim162@naver.com>
2022-05-26 19:42:20 +0900

    귀여운 민혀기 대체

--- app/src/main/java/com/example/lastommg/MypageActivity.java
+++ app/src/main/java/com/example/lastommg/MypageActivity.java
@@ -41,7 +41,7 @@ import java.io.Serializable;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 // 앨범 클릭 추가할때 ,setOnclick implements해야함
-public class MypageActivity extends AppCompatActivity implements AlbumAdapter.OnItemClickListener, Serializable {
+public class MypageActivity extends AppCompatActivity implements AlbumAdapter.OnItemClickListener, Serializable, View.OnClickListener {
 
     private Context mContext;
     private RecyclerView my_album;
@@ -53,6 +53,7 @@ public class MypageActivity extends AppCompatActivity implements AlbumAdapter.On
     private static final int PICK_FROM_ALBUM = 1;
     private static final int CROP_FROM_CAMERA = 2;
     private Uri mImageCaptureUri;
+    RoundImageView mPressProfileImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -64,9 +65,9 @@ public class MypageActivity extends AppCompatActivity implements AlbumAdapter.On
         //프로필 이미지 띄우기(동그랗게)
         Uri a;
         a = Uri.parse(local.getPro_img());
-        RoundImageView mPressProfileImg = findViewById(R.id.round_profile_image);
+        mPressProfileImg = findViewById(R.id.round_profile_image);
         Glide.with(MypageActivity.this).load(a).into(mPressProfileImg);
-       // mPressProfileImg.setOnClickListener(this);
+        mPressProfileImg.setOnClickListener(this);
 
 //        RoundImageView riv = findViewById(R.id.round_profile_image);
 //        Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.profile_img);
@@ -103,100 +104,105 @@ public class MypageActivity extends AppCompatActivity implements AlbumAdapter.On
         });
     }
     //프로필 이미지 설정 methods/////////////////////////////////////////////////////////////////
-    /**
-     * 앨범에서 이미지 가져오기
-     * private void doTakeAlbumAction()
-     *     {
-     *         // 앨범 호출
-     *         Intent intent = new Intent(Intent.ACTION_PICK);
-     *         intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
-     *         startActivityForResult(intent, PICK_FROM_ALBUM);
-     *     }
-     *
-     *     @Override
-     *     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-     *         super.onActivityResult(requestCode, resultCode, data);
-     *         if (resultCode != RESULT_OK) {
-     *             return;
-     *         }
-     *
-     *         switch (requestCode) {
-     *             case CROP_FROM_CAMERA: {
-     *                 // 크롭이 된 이후의 이미지를 넘겨 받습니다.
-     *                 // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
-     *                 // 임시 파일을 삭제합니다.
-     *                 final Bundle extras = data.getExtras();
-     *
-     *                 if (extras != null) {
-     *                     Bitmap photo = extras.getParcelable("data");
-     *                     mPressProfileImg.setImageBitmap(photo);
-     *                 }
-     *
-     *                 // 임시 파일 삭제
-     *                 File f = new File(mImageCaptureUri.getPath());
-     *                 if (f.exists()) {
-     *                     f.delete();
-     *                 }
-     *
-     *                 break;
-     *             }
-     *
-     *             case PICK_FROM_ALBUM: {
-     *                 // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
-     *                 // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
-     *
-     *                 mImageCaptureUri = data.getData();
-     *             }
-     *
-     *             case PICK_FROM_CAMERA: {
-     *
-     *
-     *                 // 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
-     *                 // 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
-     *
-     *                 Intent intent = new Intent("com.android.camera.action.CROP");
-     *                 intent.setDataAndType(mImageCaptureUri, "image/*");
-     *
-     *                 intent.putExtra("outputX", 90);
-     *                 intent.putExtra("outputY", 90);
-     *                 intent.putExtra("aspectX", 1);
-     *                 intent.putExtra("aspectY", 1);
-     *                 intent.putExtra("scale", true);
-     *                 intent.putExtra("return-data", true);
-     *                 startActivityForResult(intent, CROP_FROM_CAMERA);
-     *                 break;
-     *             }
-     *         }
-     *     }
-     *
-     *     //프로필 이미지 눌렀을 때
-     *     @Override
-     *     public void onClick(View view) {
-     *         DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
-     *         {
-     *             @Override
-     *             public void onClick(DialogInterface dialog, int which)
-     *             {
-     *                 doTakeAlbumAction();
-     *             }
-     *         };
-     *
-     *         DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
-     *         {
-     *             @Override
-     *             public void onClick(DialogInterface dialog, int which)
-     *             {
-     *                 dialog.dismiss();
-     *             }
-     *         };
-     *
-     *         new AlertDialog.Builder(this)
-     *                 .setTitle("업로드할 이미지 선택")
-     *                 .setNeutralButton("앨범선택", albumListener)
-     *                 .setNegativeButton("취소", cancelListener)
-     *                 .show();
-     *     }
-     */
+
+      //앨범에서 이미지 가져오기
+      private void doTakeAlbumAction()
+      {
+          // 앨범 호출
+          Intent intent = new Intent(Intent.ACTION_PICK);
+          intent.setType(android.provider.MediaStore.Images.Media.CONTENT_TYPE);
+          startActivityForResult(intent, PICK_FROM_ALBUM);
+      }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode != RESULT_OK) {
+            return;
+        }
+
+        switch (requestCode) {
+            case CROP_FROM_CAMERA: {
+                // 크롭이 된 이후의 이미지를 넘겨 받습니다.
+                // 이미지뷰에 이미지를 보여준다거나 부가적인 작업 이후에
+                // 임시 파일을 삭제합니다.
+                final Bundle extras = data.getExtras();
+
+                if (extras != null) {
+                    Bitmap photo = extras.getParcelable("data");
+                    mPressProfileImg.setImageBitmap(photo);
+                }
+
+                // 임시 파일 삭제
+                File f = new File(mImageCaptureUri.getPath());
+                if (f.exists()) {
+                    f.delete();
+                }
+
+                break;
+            }
+
+            case PICK_FROM_ALBUM: {
+                // 이후의 처리가 카메라와 같으므로 일단  break없이 진행합니다.
+                // 실제 코드에서는 좀더 합리적인 방법을 선택하시기 바랍니다.
+                if (data.getData() != null) {
+                    try {
+                        mImageCaptureUri = data.getData();
+                        Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), mImageCaptureUri);
+                        mPressProfileImg.setImageBitmap(bitmap);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+
+            case PICK_FROM_CAMERA: {
+//                 이미지를 가져온 이후의 리사이즈할 이미지 크기를 결정합니다.
+//                 이후에 이미지 크롭 어플리케이션을 호출하게 됩니다.
+
+                Intent intent = new Intent("com.android.camera.action.CROP");
+                intent.setDataAndType(mImageCaptureUri, "image/*");
+
+                intent.putExtra("outputX", 90);
+                intent.putExtra("outputY", 90);
+                intent.putExtra("aspectX", 1);
+                intent.putExtra("aspectY", 1);
+                intent.putExtra("scale", true);
+                intent.putExtra("return-data", true);
+                startActivityForResult(intent, CROP_FROM_CAMERA);
+                break;
+            }
+        }
+    }
+
+          //프로필 이미지 눌렀을 때
+          @Override
+          public void onClick(View view) {
+              DialogInterface.OnClickListener albumListener = new DialogInterface.OnClickListener()
+              {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which)
+                  {
+                      doTakeAlbumAction();
+                  }
+              };
+
+              DialogInterface.OnClickListener cancelListener = new DialogInterface.OnClickListener()
+              {
+                  @Override
+                  public void onClick(DialogInterface dialog, int which)
+                  {
+                      dialog.dismiss();
+                  }
+              };
+
+              new AlertDialog.Builder(this)
+                      .setTitle("업로드할 이미지 선택")
+                      .setNeutralButton("앨범선택", albumListener)
+                      .setNegativeButton("취소", cancelListener)
+                      .show();
+          }
+
 
 
     //밑에 사진 그리드 띄우기/////////////////////////////////////////////////////////////////////////////////////////
@@ -222,8 +228,5 @@ public class MypageActivity extends AppCompatActivity implements AlbumAdapter.On
 
         startActivity(intent, optionsCompat.toBundle());
     }
-
-
-
 }
 
